45eb75864fbd79f04942e3cd855f20e1487a38e6
ayoubaitarab <ayoubaitarab20@gmail.com>
2024-09-02 17:15:25 +0200

    hero image

--- app/(landing)/_components/hero.tsx
+++ app/(landing)/_components/hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
         <div className='text-center'>
             <h2 className='text-3xl font-bold w-full md:w-[600px] mb-3'>Discover And Share Inspring Carrer Journies</h2>
             <p className='w-full md:w-[500px] mx-auto mb-3'>StoryMaker is an innovative platform designed for storytellers of all kinds. Whether you're a seasoned writer or a beginner with a story to tell, StoryMaker provides a vibrant community and a creative space to craft, share, and discover stories.</p>
-            <Image src="/hero.JPG" alt='hero' className='mx-auto' width={500} height={300}/>
+            <Image src="/hero.jpg" alt='hero' className='mx-auto' width={500} height={300}/>
         </div>
     )
 }
